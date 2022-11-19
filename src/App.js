866f7e371aef426a1803605cbda7247ca91899f6
Prateek Varma <prtk.varma@gmail.com>
2022-11-18 23:00:25 -0600

    Installed react-bootstrap and implemented some styling on app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import CsvData from './components/CsvData';
-import CsvToPie from './components/CsvToPie';
+import TallMenBarChart from "./components/TallMenBarChart";
+
+import { Container, Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Paragraph
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-        >
-          Link
-        </a>
-      </header>
-      <CsvToPie />
-      <CsvData />
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand>D3.js and React</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <TallMenBarChart />
+      </Container>
     </div>
   );
 }
